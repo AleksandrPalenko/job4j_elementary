9ee2279afd00f5046469c1e654166551998e62f8
Aleksandr Palenko <palenko.as@mail.ru>
2021-03-12 13:45:14 +0300

    3.1. Загрузка файлов на github [#496158]

---
+++ src/ru/job4j/Multiple.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+public class Multiple {
+    public static void main(String[] args) {
+        int i = 1;
+        while(i<10){
+            System.out.println("1 * " + i+" = "+i*1);
+            i++;
+        }
+    }
+}
