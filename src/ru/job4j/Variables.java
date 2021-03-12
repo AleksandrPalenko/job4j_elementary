dbf2f07d16807410e2557805cdc4a0b44e7eca94
Aleksandr Palenko <palenko.as@mail.ru>
2021-03-12 18:39:36 +0300

    1. Определите тип данных для переменных[#496136]

--- src/ru/job4j/Variables.java
+++ src/ru/job4j/Variables.java
@@ -2,14 +2,14 @@ package ru.job4j;
 
 public class Variables {
     public static void main(String[] args) {
-        byte age;                   //все переменные имеют примитивный тип данных
-        char sex;
-        int high;
-        long numberOfBricks;
-        float numberOfBacterias;
-        double distanceToSpaceObjects;
-        boolean workPermit;
-        short sizeOfFile;
+        byte age = 26;                   //целочисленный тип данных
+        char gender = 'M';               //символьный тип данных
+        double high = 176.6;                    //вещественный тип данных
+        long numberOfBricks = 999564654;        //целочисленный тип данных
+        float numberOfBacterias = 5554984.54F;      //вещественный тип данных
+        double distanceToSpaceObjects = 5355488.4647846;        //вещественный тип данных
+        boolean workPermit = true;          //логический тип данных
+        short sizeOfFile = 1064;            //целочисленный тип данных
 
     }
 }
