1bccb4d53de7065ccdd8d64f719872043d8afe74
Aleksandr Palenko <palenko.as@mail.ru>
2021-03-13 21:31:29 +0300

    3.3. Разбор первой программы. [#496152]

--- src/ru/job4j/Multiple.java
+++ src/ru/job4j/Multiple.java
@@ -3,7 +3,7 @@ package ru.job4j;
 public class Multiple {
     public static void main(String[] args) {
         int i = 1;
-        while(i<10){
+        while(i<=9){
             System.out.println("1 * " + i+" = "+i*1);
             i++;
         }
