55066914c3d208e5111db97d7af3a9340fa3cfb8
Aleksandr Palenko <palenko.as@mail.ru>
2021-03-13 21:43:46 +0300

    3. Арифметические операции. [#496145]

--- src/ru/job4j/Variables.java
+++ src/ru/job4j/Variables.java
@@ -4,7 +4,7 @@ public class Variables {
     public static void main(String[] args) {
         byte age = 26;                   //целочисленный тип данных
         char gender = 'M';               //символьный тип данных
-        double high = 176.6;                    //вещественный тип данных
+        short height = 176;                    //вещественный тип данных
         long numberOfBricks = 999564654;        //целочисленный тип данных
         float numberOfBacterias = 5554984.54F;      //вещественный тип данных
         double distanceToSpaceObjects = 5355488.4647846;        //вещественный тип данных
--- src/ru/job4j/calculator/Calculator.java
+++ src/ru/job4j/calculator/Calculator.java
@@ -1,14 +1,17 @@
 package ru.job4j.calculator;
-class MathFunc {
-
-    public static int func1(int x) {
-        int y = x * x - 50;
-        return y;
-    }
 
 
+public class Calculator {
     public static void main(String[] args) {
-        int result = MathFunc.func1(100);
-        System.out.println(result);
+        int two = 2;
+        int six = 6;
+        int four = 4;
+        int five = 5;
+        int sixDivTwo = six / two;
+        int fiveMinusTwo = five - two;
+        int fourTimeTwo = four * two;
+        System.out.println(sixDivTwo);
+        System.out.println(fiveMinusTwo);
+        System.out.println(fourTimeTwo);
     }
 }
