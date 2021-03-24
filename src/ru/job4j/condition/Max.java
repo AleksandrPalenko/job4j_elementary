3abbd193970c8d5456621a755f6779091a75d0e6
Aleksandr Palenko <palenko.as@mail.ru>
2021-03-24 17:51:45 +0300

    4.2. Тернарное сравнение [#496092]

---
+++ src/ru/job4j/condition/Max.java
@@ -0,0 +1,9 @@
+package ru.job4j.condition;
+
+public class Max {
+    public static int max(int left, int right) {
+        boolean maxMembers = left > right;
+        int result = maxMembers ? left : right;
+        return result;
+    }
+}
