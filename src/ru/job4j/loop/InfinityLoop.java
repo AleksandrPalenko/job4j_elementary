739cf7a80f67cce540c250e57de9690a396b3638
AleksandrPalenko <palenko.as@mail.ru>
2021-04-05 15:30:04 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#496197]

---
+++ src/ru/job4j/loop/InfinityLoop.java
@@ -0,0 +1,11 @@
+package ru.job4j.loop;
+
+public class InfinityLoop {
+    public static void main(String[] args) {
+        int i = 0;
+        while (true) {
+            System.out.println("78");
+            i++;
+        }
+    }
+}
