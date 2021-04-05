8ce41eb124e3755118aa119e4564a3fd76976b22
AleksandrPalenko <palenko.as@mail.ru>
2021-04-05 10:21:47 +0300

    4.4. Вложенный оператор if else [#496093]

--- test/ru/job4j/condition/DivideBySixTest.java
+++ test/ru/job4j/condition/DivideBySixTest.java
@@ -22,7 +22,7 @@ public class DivideBySixTest {
 
     @Test
     public void whenNumberNoDivideBy3AndEven() {
-        int in = 10;
+        int in = 14;
         String result = DivideBySix.checkNumber(in);
         String expected = "Исходное число не делится на 3, но является четным.";
         Assert.assertEquals(expected, result);
@@ -30,7 +30,7 @@ public class DivideBySixTest {
 
     @Test
     public void whenNumberNoDivideBy3AndNotEven() {
-        int in = 23;
+        int in = 25;
         String result = DivideBySix.checkNumber(in);
         String expected = "Исходное число не делится на 3 и не является четным.";
         Assert.assertEquals(expected, result);
