db9fd9e2260a207b70ec23a1f5aeef2c930efc17
AleksandrPalenko <palenko.as@mail.ru>
2021-04-09 13:42:48 +0300

    5.7. Ипотека [#496191]

--- src/ru/job4j/loop/Mortgage.java
+++ src/ru/job4j/loop/Mortgage.java
@@ -3,9 +3,8 @@ package ru.job4j.loop;
 public class Mortgage {
     public static int year(int amount, int salary, int percent) {
         int year = 0;
-        int a = 0;
         while (amount > 0) {
-            amount = (amount + percent) - salary;
+            amount = amount + (amount * percent / 100) - salary;
             year++;
 
         }
