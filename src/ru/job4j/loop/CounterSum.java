fd3e98110201ab8d62b50cf0e9135c414a7d2193
AleksandrPalenko <palenko.as@mail.ru>
2021-04-26 11:02:11 +0300

    rename

--- src/ru/job4j/loop/CounterSum.java
+++ src/ru/job4j/loop/CounterSum.java
@@ -1,6 +1,6 @@
 package ru.job4j.loop;
 
-public class Counter {
+public class CounterSum {
     public static int sum(int start, int finish) {
         int sum = 0;
         for (int i = start; i <= finish; i++) {
